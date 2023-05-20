b1b2868fac7746b807fd456d3df85849456dcea1
chuprov-vd <chuprov271@gmail.com>
2023-05-20 17:08:04 +0300

    add decision 2 and 3

--- main.py
+++ main.py
@@ -6,3 +6,21 @@
 num = input("Введите число: ")
 result = int(num[0]) + int(num[1]) + int(num[2])
 print(num, "-> {} ({} + {} + {})".format(result, num[0], num[1], num[2]))
+
+# Решение 2:
+num = int(input("Введите число: "))
+inter_num = 0
+while num > 0:
+    n = num % 10
+    inter_num = inter_num + n
+    num = num // 10
+print(inter_num)
+
+# Решение 3:
+num = input("Введите число: ")
+while len(num) != 3:
+    num = input("Введите 3-х значное число")
+num1 = 0
+for i in num:
+    num1 = int(i) + num1
+print(num1)
