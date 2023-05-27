7e8ed02b9a2e62ae5b44435e361431b10a7daa9e
chuprov-vd <chuprov271@gmail.com>
2023-05-27 23:27:21 +0300

    creating a neural network

---
+++ neural_network.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+weights = np.array([0.1, 0.2, 0])
+
+
+def neural_network(put, weights):
+    pred = put.dot(weights)  # dot высчитывание скалярного произведения
+    return pred
+
+
+toes = np.array([8.5, 9.5, 9.9, 9.0])
+
+wlrec = np.array([0.65, 0.8, 0.8, 0.9])
+nfans = np.array([1.2, 1.3, 0.5, 1.0])
+put = np.array([toes[0], wlrec[0], nfans[0]])
+pred = neural_network(put, weights)
+print(pred)
