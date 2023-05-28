4f20f753502d602659cb02493488d6116c08f218
chuprov-vd <chuprov271@gmail.com>
2023-05-28 13:48:29 +0300

    Code optimization and error correction

--- task1#.py
+++ task1#.py
@@ -10,21 +10,18 @@
 
 import random
 a = []
-num = random.randint(1, 9) # генерируем список
-for i in range(num): # добавляем в список новый элемент
+num = random.randint(1, 9)  # генерируем размер списка
+for i in range(num):    # добавляем в список новый элемент
     x = random.randint(1, 9)
     a.insert(i, x)
     result_list = []
-print(a) # печатаем полученный список
-quantity = len(a)
-num = 1
-if quantity % 2 == 0: # проверяем на четность для правильного произведения
-    for i in range(quantity // 2):
-        num = num + i
-        result_list.insert(i, a[i] * a[-num])
-else:
-    for i in range(quantity // 2):
-        num = num + i
-        result_list.insert(i, a[i] * a[-num])
-    result_list.insert(i + 1, a[quantity // 2] * a[quantity // 2]) # умножаем число само на себя так как нет пары
+print(a)    # печатаем полученный список
+quantity = len(a)   # размер списка а
+num = 1     # так как переменная num больше не требуется, кладем в нее новые данные
+
+for i in range(quantity // 2):
+    result_list.insert(i, a[i] * a[-num])
+    num += 1
+if quantity % 2 != 0:   # проверяем на четность для правильного произведения
+    result_list.insert(i + 1, a[quantity // 2] ** 2)  # умножаем число само на себя так как нет пары
 print(result_list)
